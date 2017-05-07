78ee3f285a8744650ca49f9f1a5a3244c5d992a2
amir-arad <amira@wix.com>
2017-05-08 01:05:41 +0300

    add spaceship

--- index.vr.js
+++ index.vr.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {
+	PointLight,
+	Model,
   AppRegistry,
   asset,
   StyleSheet,
@@ -15,20 +17,15 @@ export default class proto extends React.Component {
         <Pano source={[asset('starsRight.png'), asset('starsLeft.png'),
             asset('starsTop.png'), asset('starsBottom.png'),
             asset('starsFront.png'), asset('starsBack.png')]}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+          <PointLight />
+          <Model
+              lit={true}
+              style={{ transform : [   {translate : [0,0,-100]}, { scale : 1 }]}}
+              texture={asset('space_frigate_6_color.png')}
+              source={{
+				  obj: asset('space_frigate_6.obj')
+			  }}
+          />
       </View>
     );
   }
