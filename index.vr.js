24e18fb3015cb736f925542a0476726bc8a5779f
tomr <tomr@wix.com>
2017-05-10 19:57:24 +0300

    added component that allows debugging of xbox controller

--- index.vr.js
+++ index.vr.js
@@ -8,25 +8,29 @@ import {
   Pano,
   Text,
   View,
+  NativeModules
 } from 'react-vr';
+import {ControllerState} from './components/controller-state';
 
 export default class proto extends React.Component {
   render() {
     return (
+      // <View onInput={event => console.log(event.nativeEvent.inputEvent)}>
       <View>
         <Pano source={[asset('stars/right.png'), asset('stars/left.png'),
             asset('stars/top.png'), asset('stars/bottom.png'),
-            asset('stars/front.png'), asset('stars/back.png')]}/>
-          <PointLight />
-          <Model
-              lit={true}
-              style={{ transform : [   {translate : [0,0,-100]}, { scale : 1 }]}}
-              texture={asset('space_frigate_6_color.png')}
-              source={{
-				  obj: asset('space_frigate_6.obj')
-			  }}
-          />
-      </View>
+            asset('stars/front.png'), asset('stars/back.png')]} />
+        <PointLight />
+        {/*<ControllerState/>*/}
+        <Model
+          lit={true}
+          style={{ transform : [   {translate : [0,0,-10]}, { scale : 1 }]}}
+          texture={asset('space_frigate_6_color.png')}
+          source={{
+            obj: asset('space_frigate_6.obj')
+          }}
+        />
+      </View> 
     );
   }
 };
