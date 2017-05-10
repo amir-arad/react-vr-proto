62fee8e9215d60d440f3cefa1cec5983d652d350
Nir Benita <benitanir@gmail.com>
2017-05-10 17:54:22 +0300

    spring works

--- index.vr.js
+++ index.vr.js
@@ -11,62 +11,61 @@ import {
   Animated,
 } from 'react-vr';
 
-class Playground extends React.Component {
+export default class proto extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bounceValue: new Animated.Value(0),
+      rotateYValue: new Animated.Value(0),
     };
   }
-  render() {
-    return (
-      <Animated.Image                         // Base: Image, Text, View
-        source={
-          asset('starsFront.png')
-        }
-        style={{
-          flex: 1,
-          width: 1,
-          height: 1,
-          transform: [                        // `transform` is an ordered array
-            {scale: this.state.bounceValue},  // Map `bounceValue` to `scale`
-          ]
-        }}
-      />
-    );
-  }
+  
   componentDidMount() {
-    this.state.bounceValue.setValue(1.5);     // Start large
+    // this.state.rotateYValue.setValue(0);     // Start large
     Animated.spring(                          // Base: spring, decay, timing
-      this.state.bounceValue,                 // Animate `bounceValue`
+      this.state.rotateYValue,                 // Animate `bounceValue`
       {
-        toValue: 0.8,                         // Animate to smaller size
-        friction: 1,                          // Bouncier spring
+        toValue: 30,                         // Animate to smaller size
+        friction: 1,
+        tension: 60                          // Bouncier spring
       }
     ).start();                                // Start the animation
   }
-}
 
-export default class proto extends React.Component {
   render() {
     return (
       <View>
-
         <Pano source={[asset('stars/right.png'), asset('stars/left.png'),
             asset('stars/top.png'), asset('stars/bottom.png'),
             asset('stars/front.png'), asset('stars/back.png')]}/>
           <PointLight />
-          <Model
-              lit={true}
-              style={{ transform : [   {translate : [0,0,-100]}, { scale : 1 }]}}
-              texture={asset('space_frigate_6_color.png')}
-              source={{
-				  obj: asset('space_frigate_6.obj')
-			  }}
-          />
+            <View style={{
+              transform : [
+                {translate : [0,0,-70]},
+                { scale : 1 }
+              ],
+              flexDirection: 'column',
+              height: 1,
+              padding: 0.2 }}>
+              <Animated.View 
+                style={{
+                  transform : [
+                    {rotateY: this.state.rotateYValue},
+                    {translate : [0,0,0] },
+                    { scale : 1 }
+                  ] }}>
+                <Model
+                lit={true}
+                style={{ transform : [{translate : [0,0,0]}, { scale : 1 }]}}
+                texture={asset('space_frigate_6_color.png')}
+                source={{
+                  obj: asset('space_frigate_6.obj')
+                }} />
+              </Animated.View>
+              
+            </View>
       </View>
     );
   }
 };
 
-AppRegistry.registerComponent('Playground', () => Playground);
+AppRegistry.registerComponent('proto', () => proto);
