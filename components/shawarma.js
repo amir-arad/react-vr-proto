6c31d980a21449a19884f81dec6d0986c6e400f9
amir-arad <amira@wix.com>
2017-05-11 19:33:40 +0300

    make shawarma pause when observed

--- components/shawarma.js
+++ components/shawarma.js
@@ -1,51 +1,60 @@
 import React from "react";
-import { View, Animated } from "react-vr";
+import { View, Animated} from "react-vr";
 
 
 export default class Shawarma extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rotateYValue: new Animated.Value(0),
-    };
-    this.rotateModel = this.rotateModel.bind(this);
-  }
-  rotateModel() {
-    this.state.rotateYValue.setValue(0); // Start large
-    Animated.timing(
-      // Base: spring, decay, timing
-      this.state.rotateYValue, // Animate `bounceValue`
-      {
-        toValue: 360, // Animate to smaller size
-        duration: 4000,
-      }
-    ).start(this.rotateModel); // Start the animation
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			rotateYValue: new Animated.Value(0),
+		};
 
-  componentDidMount() {
-    this.rotateModel();
-  }
-  
-  render() {
-    return (
-    <View
-      style={{
-        transform: [{ translate: [0, 0, this.state.zoom] }, { scale: 1 }],
-        flexDirection: "column",
-        height: 1,
-        padding: 0.2
-      }}>
-      <Animated.View
-        style={{
-          transform: [
-            { rotateY: this.state.rotateYValue },
-            { translate: [0, 0, 0] },
-            { scale: 1 }
-          ]
-        }}>
-        {this.props.children}
-      </Animated.View>
-    </View>
-    );
-  }
+	}
+	rotateModel(){
+		this.state.rotateYValue.setValue(this.state.rotateYValue.toJSON() % 360); // Start large
+		this.animation = Animated.timing(
+			// Base: spring, decay, timing
+			this.state.rotateYValue, // Animate `bounceValue`
+			{
+				easing: (i)=>i,
+				toValue: 360, // Animate to smaller size
+				duration: 4000 * (360 - this.state.rotateYValue.toJSON()) / 360,
+			}
+		);
+		this.animation.start(this.onAnimationEnd); // Start the animation
+	};
+
+	onAnimationEnd = (event) => {
+		if (event && event.finished) {
+			this.rotateModel();
+		}
+	};
+	componentDidMount() {
+		this.rotateModel();
+	}
+
+	render() {
+		return (
+            <View
+                style={{
+					transform: [{ translate: [0, 0, this.state.zoom] }, { scale: 1 }],
+					flexDirection: "column",
+					height: 1,
+					padding: 0.2
+				}}>
+                <Animated.View
+                    onEnter={()=>this.animation.stop()}
+                    onExit={()=>this.rotateModel()}
+                    style={{
+						transform: [
+							{ rotateY: this.state.rotateYValue },
+							{ translate: [0, 0, 0] },
+							{ scale: 1 }
+						]
+					}}>
+					{this.props.children}
+                </Animated.View>
+            </View>
+		);
+	}
 }
