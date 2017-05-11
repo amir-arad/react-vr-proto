c565bd9bab6342868b8717334265fb675e11177e
Nir Benita <benitanir@gmail.com>
2017-05-11 14:56:05 +0300

    zoom buttons working

--- index.vr.js
+++ index.vr.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './vr/components/button';
 import {
 	PointLight,
 	Model,
@@ -16,61 +17,86 @@ export default class proto extends React.Component {
     super(props);
     this.state = {
       rotateYValue: new Animated.Value(0),
+      zoom: -70,
     };
-    this.rotateModel = this
-      .rotateModel
-      .bind(this);
+    this.rotateModel = this.rotateModel.bind(this);
   }
-  
+
   rotateModel() {
-    this.state.rotateYValue.setValue(0);     // Start large
-    Animated.timing(                          // Base: spring, decay, timing
-      this.state.rotateYValue,                 // Animate `bounceValue`
+    this.state.rotateYValue.setValue(0); // Start large
+    Animated.timing(
+      // Base: spring, decay, timing
+      this.state.rotateYValue, // Animate `bounceValue`
       {
-        toValue: 360,                         // Animate to smaller size
+        toValue: 360, // Animate to smaller size
         duration: 4000,
       }
-    ).start(this.rotateModel);                                // Start the animation
-
+    ).start(this.rotateModel); // Start the animation
   }
   componentDidMount() {
-    this.rotateModel()
+    this.rotateModel();
   }
-
+  
   render() {
     return (
       <View>
-        <Pano source={[asset('stars/right.png'), asset('stars/left.png'),
-            asset('stars/top.png'), asset('stars/bottom.png'),
-            asset('stars/front.png'), asset('stars/back.png')]}/>
-          <PointLight />
-            <View style={{
-              transform : [
-                {translate : [0,0,-70]},
-                { scale : 1 }
-              ],
-              flexDirection: 'column',
-              height: 1,
-              padding: 0.2 }}>
-              <Animated.View 
-                style={{
-                  transform : [
-                    {rotateY: this.state.rotateYValue},
-                    {translate : [0,0,0] },
-                    { scale : 1 }
-                  ] }}>
-                <Model
-                lit={true}
-                style={{ transform : [{translate : [0,0,0]}, { scale : 1 }]}}
-                texture={asset('space_frigate_6_color.png')}
-                source={{
-                  obj: asset('space_frigate_6.obj')
-                }} />
-              </Animated.View>
-            </View>
+        <Pano
+          source={[
+            asset("stars/right.png"),
+            asset("stars/left.png"),
+            asset("stars/top.png"),
+            asset("stars/bottom.png"),
+            asset("stars/front.png"),
+            asset("stars/back.png")
+          ]}
+        />
+        <PointLight />
+        <View style={{ transform: [{translate: [-2,0,-4]}]}} >
+          <Button
+            text="+"
+            callback={() => {
+              console.log(this.state.zoom);
+              this.setState(prevState => ({ zoom: prevState.zoom - 5 }));
+              }}
+          />
+
+          <Button
+            text="-"
+            callback={() => {
+              console.log(this.state.zoom);
+              this.setState(prevState => ({ zoom: prevState.zoom + 5 }));
+              }}
+          />
+        </View>
+        <View
+          style={{
+            transform: [{ translate: [0, 0, this.state.zoom] }, { scale: 1 }],
+            flexDirection: "column",
+            height: 1,
+            padding: 0.2
+          }}>
+          <Animated.View
+            style={{
+              transform: [
+                { rotateY: this.state.rotateYValue },
+                { translate: [0, 0, 0] },
+                { scale: 1 }
+              ]
+            }}>
+            <Model
+              lit={true}
+              style={{ transform: [{ translate: [0, -5, 0] }, { scale: 1 }] }}
+              texture={asset("space_frigate_6_color.png")}
+              source={{
+                obj: asset("space_frigate_6.obj")
+              }}
+            />
+          </Animated.View>
+        </View>
       </View>
     );
   }
-};
+}
+
 
 AppRegistry.registerComponent('proto', () => proto);
